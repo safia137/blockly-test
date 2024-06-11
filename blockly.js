3324075739608399240cfb696cead85e1587ebed
safia137 <somaligirl_8@hotmail.com>
2024-06-12 01:05:13 +0200

    workspace-blok

--- blockly.js
+++ blockly.js
@@ -21,3 +21,50 @@ Blockly.Blocks['proces'] = {
       this.setHelpUrl("");
     }
   };
+
+  Blockly.Blocks['werkstroom'] = {
+    init() {
+      this.appendDummyInput()
+        .appendField("Werkstroom")
+        .appendField(new Blockly.FieldDropdown(commonFields["Workflow Names"].call(this, 'NL')), "ACTION");
+      this.appendDummyInput()
+        .appendField("Taal")
+        .appendField(new Blockly.FieldDropdown(commonFields["Taal"], function(language) {
+          updateDetails(this.sourceBlock_, 'workflowName', language);
+        }), "LANGUAGE");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour("#0000FF");
+      this.setTooltip("Represents a workflow.");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks['zichtbaar'] = {
+    init() {
+      this.appendDummyInput()
+        .appendField("Zichtbaar")
+        .appendField("Stap")
+        .appendField(new Blockly.FieldCheckbox("TRUE", function(checked) {
+          const stepBlocks = this.sourceBlock_.getChildren(false).filter(block => block.type === 'stap');
+          stepBlocks.forEach(block => {
+            const correspondingJson = jsonData.find(item => item.stepOrder.toString() === block.getFieldValue('NUMBER'));
+            if (correspondingJson) correspondingJson.StepIsVisible = checked;
+          });
+        }), "STEP_VISIBLE")
+        .appendField("Fase")
+        .appendField(new Blockly.FieldCheckbox("TRUE", function(checked) {
+          const phaseBlocks = this.sourceBlock_.getChildren(false).filter(block => block.type === 'fase');
+          phaseBlocks.forEach(block => {
+            const correspondingJson = jsonData.find(item => item.phaseOrder.toString() === block.getFieldValue('NUMBER'));
+            if (correspondingJson) correspondingJson.PhaseIsVisible = checked;
+          });
+        }), "FASE_VISIBLE");
+      this.appendStatementInput("LOOP_BODY").setCheck(null);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour("#FFA500");
+      this.setTooltip("Indicates visibility.");
+      this.setHelpUrl("");
+    }
+  };
